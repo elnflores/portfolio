d19f22944aaa4a19b77cda7116ea18c8e79769ab
Elijah Flores <elnflores10@gmail.com>
2025-08-08 16:39:02 -0500

    Added education and website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Elijah Flores',
+    template: '%s | Elijah Flores',
   },
   description: 'This is my portfolio.',
   openGraph: {
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -9,13 +9,27 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-xl mb-4 tracking-tighter">
-        Publications
+      <h1 className="font-semibold text-2xl mb-4 tracking-tighter">
+        Education
       </h1>
+      <div className="w-full flex flex-col space-x-0 md:space-x-2">
+        <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+          {formatDate('2026-05-01', false, false)}
+        </p>
+        <a href="https://www.cs.utexas.edu/"
+           className="text-neutral-900 dark:text-neutral-100 mb-8 tracking-tight"
+        >
+          B.S. Computer Science, University of Texas at Austin.
+        </a>
+      </div>
 
-      <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+
+      <h2 className="font-semibold text-2xl mb-4 tracking-tighter">
+        Publications
+      </h2>
+      <div className="w-full flex flex-col space-x-0 md:space-x-2">
         <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-          {formatDate('2024-07-08', false, false)}
+          {formatDate('2024-07-01', false, false)}
         </p>
         <a href="https://iopscience.iop.org/article/10.3847/2515-5172/ad686e"
            className="text-neutral-900 dark:text-neutral-100 tracking-tight"
